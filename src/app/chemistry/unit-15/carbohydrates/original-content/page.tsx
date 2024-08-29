f21b50287e5c045819302512d3f23ca0488c6ab9
tahaamin2013 <tahaamindob2013@gmail.com>
2024-08-29 10:00:09 +0500

    husnain doing errors

--- src/app/chemistry/unit-15/carbohydrates/original-content/page.tsx
+++ src/app/chemistry/unit-15/carbohydrates/original-content/page.tsx
@@ -10,7 +10,7 @@ const BiochemistryUnit15: React.FC = () => {
                 Carbohydrates, or hydrates of carbon, are macromolecules made of carbon, hydrogen, and oxygen. The primary function of carbohydrates is to supply energy to all cells of the body. One gram of carbohydrate provides 4 kilocalories of energy.
             </p>
             <p className="mb-4">
-                Carbohydrates are broken down by the body into glucose, a type of sugar. Your body cells, tissues, and organs use glucose as fuel. When your body does not get sufficient carbohydrates, it looks for another energy source, breaking down protein in your muscles and body fat to use as energy. Glucose is the preferred fuel for the brain, which can't simply use other fuel resources like fat or protein for energy.
+                Carbohydrates are broken down by the body into glucose, a type of sugar. Your body cells, tissues, and organs use glucose as fuel. When your body does not get sufficient carbohydrates, it looks for another energy source, breaking down protein in your muscles and body fat to use as energy. Glucose is the preferred fuel for the brain, which can&apos;t simply use other fuel resources like fat or protein for energy.
             </p>
             <p className="mb-4">
                 <strong>DO YOU KNOW!</strong> A calorie is a unit used to measure the energy in your food. A calorie is the energy required to raise the temperature of 1 gram of water by 1 degree Celsius. Calories are the amount of energy released when your body breaks down (digests and absorbs) food. The more calories a food has, the more energy it can provide to your body.
